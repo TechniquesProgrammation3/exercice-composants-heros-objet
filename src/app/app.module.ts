921ce6ee5e5e9a706febba02f8e5433961fa1172
Carine Croteau <croteau.carine@cegepvicto.ca>
2021-01-07 17:42:05 -0500

    ajout du CRUD en tableau

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HeroService } from './hero.service';
 
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
@@ -14,6 +17,11 @@ import { HeroComponent } from './hero/hero.component';
 import { PiedPageComponent } from './pied-page/pied-page.component';
 
 import { MatCardModule } from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import { TableHerosComponent } from './table-heros/table-heros.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,15 +32,21 @@ import { MatCardModule } from '@angular/material/card';
     RechercheComponent,
     CarouselComponent,
     HeroComponent,
-    PiedPageComponent
+    PiedPageComponent,
+    TableHerosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    FormsModule,    
+    HttpClientModule, 
+    MatCardModule,
+    MatTableModule,    
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROS } from '../mock-heros';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-page1',
@@ -8,11 +8,16 @@ import { HEROS } from '../mock-heros';
   styleUrls: ['./page1.component.css']
 })
 export class Page1Component implements OnInit {
-  heros: Hero[] = HEROS;
-  
-  constructor() { }
+  heros: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeros();
   }
 
+  getHeros(): void {
+    this.heroService.getHeros()
+        .subscribe(resultat => this.heros = resultat);
+  }
 }
